53af9ca10362bbd6a0ed9075da51d81b86216bb0
Tatehito <tatehito.st@gmail.com>
2021-04-14 18:15:37 +0900

    フロント・バックエンド間の疎通確認（ローカル環境）

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require('axios');
+const url = process.env.REACT_APP_DEV_SERVER_URL;
+
+class App extends React.Component {
+  state = {
+    title: ""
+  }
+
+  componentDidMount() {
+    axios.get(url)
+      .then((res: { data: any; }) => {
+        const title = res.data.title;
+        this.setState({ title });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App"> 
+        <header className="App-header"> 
+          <img src={logo} className="App-logo" alt="logo" /> 
+          <span>{ this.state.title }</span>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
