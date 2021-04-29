15bc2fe6f68c74c33c9e1af8a94c014df7cabf2e
Tatehito <tatehito.st@gmail.com>
2021-04-29 19:31:27 +0900

    material-table 除去

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,34 +1,8 @@
 import React, { useState } from 'react'
-import MaterialTable from 'material-table';
 
 class Home extends React.Component {
   render() {
-    const colums = [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-      },
-    ]
-    const data = [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-    ]
-
-    return (
-      <div>
-        <h2>Home</h2>
-        <MaterialTable title="Basic Sorting Preview" columns={ colums } data={ data }
-          options={{
-            sorting: true
-          }}
-        />
-      </div>
-    );
+    return <h2>Home</h2>;
   }
-  
 }
 export default Home;
